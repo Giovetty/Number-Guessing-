3d232b0fb7e3f0cbbe71bfad35a7cb33734ef818
Giovetty <kelsia20giovetty@gmail.com>
2026-06-03 17:52:46 +0100

    Added code when button is clicked

--- app/src/main/java/com/example/guessthen/MainActivity.kt
+++ app/src/main/java/com/example/guessthen/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -19,9 +20,31 @@ class MainActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
         //Link UI elements to kotlin
+
         val displayButton = findViewById<Button>(R.id.Displaybutton)
         val inputNumber = findViewById<EditText>(R.id.editTextNumber)
-        
+        val displayMessage = findViewById<TextView>(R.id.TextView)
+
+        //Add code when display button is clicked
+        displayButton?.setOnClickListener {
+            var guess = inputNumber.text.toString().toInt()
+
+            if (guess > randomNumber) {
+                displayMessage.text = "Too high!"
+            } else {
+              if (guess < randomNumber) {
+                  displayMessage.text= "Too low!"
+              } else {
+                  displayMessage.text = "Corect!!"
+              }
+            }
+
+
+
+
+
+        }
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
