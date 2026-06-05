cd58fa0131a21f46fdf6ff3d59995a1112f48523
Giovetty <kelsia20giovetty@gmail.com>
2026-06-05 15:14:02 +0100

    Added variable to store number of guesses

--- app/src/main/java/com/example/guessthen/MainActivity.kt
+++ app/src/main/java/com/example/guessthen/MainActivity.kt
@@ -14,33 +14,47 @@ import androidx.core.view.WindowInsetsCompat
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
         //Variable to store random number
         val randomNumber = (1..100).random()
 
         Log.v("Random Number", "Generated number:$randomNumber")
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+
         //Link UI elements to kotlin
 
         val displayButton = findViewById<Button>(R.id.Displaybutton)
         val inputNumber = findViewById<EditText>(R.id.editTextNumber)
         val displayMessage = findViewById<TextView>(R.id.TextView)
+// variable outside the click button otherwise every time the user click the button the guess counter would reset to zero
+        var guessQuantity = 0
 
         //Add code when display button is clicked
         displayButton?.setOnClickListener {
             var guess = inputNumber.text.toString().toInt()
 
+
+
             Toast. makeText(this,
                 "Guessing number!", Toast.LENGTH_LONG).show()
+            //Every time the use make a guess don't matter if is right or not
+            guessQuantity ++
 
-           //Added if statement to check the guess against the random number
-            if (guess > randomNumber) {
-                displayMessage.text = "Too high!"
+
+            //Added if statement to check the guess against the random number
+            if (guess == randomNumber) {
+                displayMessage.text = "Corect!!"
             } else {
-              if (guess < randomNumber) {
-                  displayMessage.text= "Too low!"
+              if (guessQuantity >=5 && guess!= randomNumber) {
+                  displayMessage.text= "Opss!! You lost !"
               } else {
-                  displayMessage.text = "Corect!!"
+                if (guess > randomNumber) {
+                  displayMessage.text = "Too high"
+                }else{
+                    displayMessage.text = "Too Low!"
+                }
+
               }
             }
 
